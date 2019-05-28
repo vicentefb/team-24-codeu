b7071ebca6056528a7c887b185ff707108d8579e
Sophie <soph.bohr22@gmail.com>
2019-05-28 13:31:42 -0500

    fixed formatting issue and completed steps 3, 4, and 5

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,120 +1,71 @@
 package com.google.codeu.servlets;
 
-
 import java.io.IOException;
 
-
 import javax.servlet.annotation.WebServlet;
-
 import javax.servlet.http.HttpServlet;
-
 import javax.servlet.http.HttpServletRequest;
-
 import javax.servlet.http.HttpServletResponse;
 
 
 import com.google.appengine.api.users.UserService;
-
 import com.google.appengine.api.users.UserServiceFactory;
-
 import com.google.codeu.data.Datastore;
-
+import com.google.codeu.data.User;
 
 /**
-
  * Handles fetching and saving user data.
-
  */
-
 @WebServlet("/about")
-
 public class AboutMeServlet extends HttpServlet {
-
-
+  
   private Datastore datastore;
-
-
- @Override
-
- public void init() {
-
-  datastore = new Datastore();
-
- }
-
- 
-
- /**
-
-  * Responds with the "about me" section for a particular user.
-
-  */
-
- @Override
-
- public void doGet(HttpServletRequest request, HttpServletResponse response)
-
-   throws IOException {
-
-
-  response.setContentType("text/html");
-
   
-
-  String user = request.getParameter("user");
-
-  
-
-  if(user == null || user.equals("")) {
-
-   // Request is invalid, return empty response
-
-   return;
-
+  @Override
+  public void init() {
+    datastore = new Datastore();
   }
 
-  
+  /**
+   * Responds with the "about me" section for a particular user.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html");
 
-  String aboutMe = "This is " + user + "'s about me.";
+    String user = request.getParameter("user");
 
-  
+    if(user == null || user.equals("")) {
+      // Request is invalid, return empty response
+      return;
+    }
 
-  response.getOutputStream().println(aboutMe);
+    User userData = datastore.getUser(user);
 
- }
+    if(userData == null || userData.getAboutMe() == null) {
+      return;
+    }
 
- 
-
- @Override
-
- public void doPost(HttpServletRequest request, HttpServletResponse response)
-
-   throws IOException {
-
-
-  UserService userService = UserServiceFactory.getUserService();  
-
-  if (!userService.isUserLoggedIn()) {
-
-   response.sendRedirect("/index.html");
-
-   return;
-
+    response.getOutputStream().println(userData.getAboutMe());
   }
 
-  
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
 
-  String userEmail = userService.getCurrentUser().getEmail();
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
 
+    String userEmail = userService.getCurrentUser().getEmail();
+    String aboutMe = request.getParameter("about-me");
 
-  System.out.println("Saving about me for " + userEmail);
+    User user = new User(userEmail, aboutMe);
+    datastore.storeUser(user);
 
-  // TODO: save the data
-
-  
-
-  response.sendRedirect("/user-page.html?user=" + userEmail);
-
- }
-
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+  }
 }
