def27f4780d7beaf79c77d5e6c99d1230ba65953
Eva St.Clair Lynch <evaslynch@Evas-MBP.austin.rr.com>
2019-06-02 22:03:45 -0500

    adding more recent changes to the master to eva-stats branch

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -1,21 +1,23 @@
 package com.google.codeu.servlets;
 
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
 import java.io.IOException;
 
+import java.util.Set;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.codeu.data.Datastore;
-import java.util.Set;
-import java.util.Iterator;
+
 /**
  * Handles fetching all users for the community page.
  */
-
 @WebServlet("/user-list")
-public class UserListServlet extends HttpServlet{
+public class UserListServlet extends HttpServlet {
+
     private Datastore datastore;
+
     @Override
     public void init() {
         datastore = new Datastore();
@@ -24,20 +26,10 @@ public class UserListServlet extends HttpServlet{
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        response.getOutputStream().println("This will be my user list:");
-        //Calling getUsers() method and storing the set into users set variable
+        response.setContentType("application/json");
         Set<String> users = datastore.getUsers();
-        //Checking if the user list is empty
-        if(users.isEmpty()) {
-            response.getOutputStream().println("The user list size is 0.");
-        }
-        else {
-            Iterator<String> it = users.iterator();
-            response.getOutputStream().println("The user list size is: " + users.size() + ".");
-            //Printing all the users in the list
-            while (it.hasNext()) {
-                response.getOutputStream().println(it.next());
-            }
-        }
+        Gson gson = new Gson();
+        String json = gson.toJson(users);
+        response.getOutputStream().println(json);
     }
 }
