85bdfcd6f23a923fd7cee280dfe503e8c9717f07
alexsieusahai <asieusah@ualberta.ca>
2019-06-14 16:24:56 -0700

    Set up basic route design

---
+++ src/main/java/com/google/codeu/data/Route.java
@@ -0,0 +1,37 @@
+package com.google.codeu.data;
+
+import java.util.List;
+
+/** 
+ * All timestamps are unix epoch time.
+ */
+public class Route	{
+	private List<String> addressList;
+	private float distanceTravelled;
+	private float departureTime;
+	private float timeSpent;
+
+	public Route(List<String> addressList, float distanceTravelled, 
+				 float departureTime, float timeSpent)	{
+		this.addressList = addressList;
+		this.distanceTravelled = distanceTravelled;
+		this.departureTime = departureTime;
+		this.timeSpent = timeSpent;
+	}
+
+	public List<String> getAddressList()	{
+		return addressList;
+	}
+
+	public float getDistanceTravelled()	{
+	   return distanceTravelled;
+	}
+
+	public float getDepartureTime()	{
+		return 	departureTime;
+	}
+	
+	public float getTimeSpent()	{
+		return timeSpent;
+	}
+}
