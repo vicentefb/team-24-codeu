e77885d9d198e06fce82296c6aef4af77b43e316
Sophie <soph.bohr22@gmail.com>
2019-06-14 14:40:43 -0700

    added first small donut chart on top

--- src/main/webapp/js/my-stats-charts.js
+++ src/main/webapp/js/my-stats-charts.js
@@ -1,9 +1,10 @@
 
-google.charts.load('current', { packages: ['corechart'] });
+google.charts.load('current', { 'packages': ['corechart', 'map'] });
 
 //loads the charts that will be created
 google.charts.setOnLoadCallback(drawDonutChart);
 google.charts.setOnLoadCallback(drawBarGraph);
+google.charts.setOnLoadCallback(drawMilesCounterDonut);
 
 /*
  * 
@@ -72,24 +73,47 @@ function drawBarGraph() {
 
     graph.draw(weekly_bar_data, graph_options);
 }
+
 /*
  * 
- * the function that determines the size of the stats circles
+ * the function that draws the miles counter donut chart
  * 
  */
-function calcCircleSizes() {
-    var sustainable_miles_week = 15; //FIXME: replace with actual data
-    var total_miles_week = 35; //FIXME: replace with actual data
-    var miles_circle_size = (sustainable_miles_week / total_miles_week) * 100;
+function drawMilesCounterDonut() {
+    //instance of DataTable for the chart
+    var totalSustainableMiles = 8.5; //FIXME: replace with actual data
+    var totalMilesGoal = 15; //will be hard coded now, they can set goal in V2
+    var restOfPie = totalMilesGoal - totalSustainableMiles;
+    if (restOfPie < 0) {
+        restOfPie = 0;
+    }
 
-    console.log("message test");
+    var weekly_miles_data = google.visualization.arrayToDataTable([
+        ['Miles Stats', 'Miles Traveled Sustainably'],
+        ['', totalSustainableMiles], //replace with actual
+        ['', restOfPie],
+    ]);
 
-    document.getElementById('miles_circle').style.height = miles_circle_size;
-    document.getElementById('miles_circle').style.width = miles_circle_size;
-    document.getElementById('miles_circle').textContent(' miles');
+    //dictionary of customization for the chart
+    var miles_circle_options = {
+        pieHole: 0.6,
+        pieSliceText: 'none',
+        //tooltip: { trigger: 'none' },
+        slices: {
+            0: { color: '#5cd65c' },
+            1: { color: 'transparent' }
+        },
+        legend: {
+            position: 'none'
+        },
 
-    //miles_circle.setAttribute("height", miles_circle_size);
-    //miles_circle.setAttribute("width", miles_circle_size);
-    //miles_circle.textContent(sustainable_miles_week + ' miles');
-    //miles_circle.textContent(' miles');
+    };
+
+    //create an instance of the type of chart (bar here)
+    var miles_chart = new google.visualization.PieChart(document.getElementById('weekly_miles_chart'));
+
+    //draw the chart
+    miles_chart.draw(weekly_miles_data, miles_circle_options);
 }
+
+
