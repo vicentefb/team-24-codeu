731bbf38fd0331dfc74f95bf74b99bbafd271578
vicentefb <chenteferrara@gmail.com>
2019-07-17 11:41:11 -0500

    Displaying colored polylines

--- src/main/webapp/js/map-loader.js
+++ src/main/webapp/js/map-loader.js
@@ -1,131 +1,160 @@
-var infoWindow;
+var map;
+var infowindow;
 var directionsService;
 var directionsDisplay;
-var map;
 function initMap() {
-    //Instantiates a directions service and display.
-    directionsService = new google.maps.DirectionsService;
-    directionsDisplay = new google.maps.DirectionsRenderer;
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: {lat: 41.85, lng: -87.65}
+  infowindow    = new google.maps.InfoWindow();
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer({
+    suppressPolylines: true,
+    infoWindow: infowindow
+  });
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: {
+      lat: 41.85,
+      lng: -87.65
+    }
+  });
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('right-panel'));
+  if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    var startInput = document.getElementById('start');
+    var startAutocomplete = new google.maps.places.Autocomplete(startInput);
+
+    startAutocomplete.addListener('place_changed', function () {
+      var place = startAutocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Select an option.");
+        return;
+      }
+
+      map.setCenter(place.geometry.location);
     });
-    var trafficLayer = new google.maps.TrafficLayer();
-    trafficLayer.setMap(map);
-    directionsDisplay.setMap(map);
-    directionsDisplay.setPanel(document.getElementById('right-panel'));
-    infoWindow = new google.maps.InfoWindow;
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
-        infoWindow.open(map);
-        map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  var startInput = document.getElementById('start');
-  var startAutocomplete = new google.maps.places.Autocomplete(startInput);
+    var endInput = document.getElementById('end');
+    var endAutocomplete = new google.maps.places.Autocomplete(endInput);
 
-  startAutocomplete.addListener('place_changed', function () {
-    var place = startAutocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Select an option.");
-      return;
-    }
+    endAutocomplete.addListener('place_changed', function () {
+      var place = endAutocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Select an option.");
+        return;
+      }
 
-    map.setCenter(place.geometry.location);
-  });
-
-  var endInput = document.getElementById('end');
-  var endAutocomplete = new google.maps.places.Autocomplete(endInput);
-
-  endAutocomplete.addListener('place_changed', function () {
-    var place = endAutocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert("Select an option.");
-      return;
-    }
-
-    map.setCenter(place.geometry.location);
-  });
-  /*
-  document.getElementById('mode').addEventListener('change', function() {
-    calculateAndDisplayRoute();
-  }); */
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                      'Error: The Geolocation service failed.' :
-                      'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
-
-// add data on how stats would be effected by route
-function calcAndDisplayStatEffect() {
-   
-}
-
-// Preload function
-var trip;
-function preload(){
-    trip = loadJSON("file.json");
+      map.setCenter(place.geometry.location);
+    });
 }
 
 function calculateAndDisplayRoute() {
-    if(directionsDisplay){
-        directionsDisplay.setMap(null);
+  var waypts = [{
+    location: "Lakewood Park, Lakechime Drive, Sunnyvale, California, EE. UU.",
+    stopover: true
+  }, {
+    location: "814 Ahwanee Ave, Sunnyvale, CA 94085, EE. UU.",
+    stopover: true
+  }, {
+     location: "1246 Birchwood Dr Sunnyvale, CA 94089",
+     stopover: true
+   }];
+  var selectedMode = document.getElementById('mode').value;
+  directionsService.route({
+    origin: "Santa Clara Marriott, Mission College Boulevard, Santa Clara, California, EE. UU.",
+    destination: "Google Building MP3, 1170 Bordeaux Dr, Sunnyvale, CA 94089, EE. UU.",
+    waypoints: waypts,
+    optimizeWaypoints: true,
+    travelMode: google.maps.TravelMode[selectedMode]
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+
+      var route = response.routes[0];
+      renderDirectionsPolylines(response, map);
+    } else {
+      window.alert('Directions request failed due to ' + status);
     }
-    var selectedMode = document.getElementById('mode').value;
-    var start = document.getElementById('start').value;
-    var end = document.getElementById('end').value;
-    directionsService.route({
-      origin: start,
-      destination: end,
-      provideRouteAlternatives: true,
-      travelMode: google.maps.TravelMode[selectedMode]
-    }, function(response, status) {
-      if (status === 'OK') {
-        directionsDisplay.setDirections(response);
-        // add a call to calcAndDisplayStatEffect() here-ish
-        for( var i=0, len = response.routes.length; i<len; i++){
-            directionsDisplay.map = map;
-            directionsDisplay.directions = response;
-            directionsDisplay.routeIndex = i;
-            directionsDisplay.setDirections(response);
-        }
-      } else {
-        window.alert('Directions request failed due to ' + status);
-      }
-    });
+  });
+  google.maps.event.addDomListener(window, "load", initMap);
 
 }
 
-// javascript to make the collapsible stats effect box reactive
+var polylineOptions = {
+  strokeColor: '#C83939',
+  strokeOpacity: 1,
+  strokeWeight: 4
+};
+var colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"];
+var polylines = [];
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
+function rainbow(numOfSteps, step) {
+    // This function generates vibrant, "evenly spaced" colours (i.e. no clustering). This is ideal for creating easily distinguishable vibrant markers in Google Maps and other apps.
+    // Adam Cole, 2011-Sept-14
+    // HSV to RBG adapted from: http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+    var r, g, b;
+    var h = step / numOfSteps;
+    var i = ~~(h * 6);
+    var f = h * 6 - i;
+    var q = 1 - f;
+    switch(i % 6){
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
     }
-  });
+    var c = "#" + ("00" + (~ ~(r * 255)).toString(16)).slice(-2) + ("00" + (~ ~(g * 255)).toString(16)).slice(-2) + ("00" + (~ ~(b * 255)).toString(16)).slice(-2);
+    return (c);
+}
+
+
+
+
+function renderDirectionsPolylines(response) {
+  var bounds = new google.maps.LatLngBounds();
+  for (var i = 0; i < polylines.length; i++) {
+    polylines[i].setMap(null);
+  }
+  var legs = response.routes[0].legs;
+  for (i = 0; i < legs.length; i++) {
+    var steps = legs[i].steps;
+    for (j = 0; j < steps.length; j++) {
+      var nextSegment = steps[j].path;
+      var stepPolyline = new google.maps.Polyline(polylineOptions);
+      var color;
+      stepPolyline.setOptions({
+        strokeColor: rainbow(legs.length, i)
+      })
+      for (k = 0; k < nextSegment.length; k++) {
+        stepPolyline.getPath().push(nextSegment[k]);
+        bounds.extend(nextSegment[k]);
+      }
+      polylines.push(stepPolyline);
+      stepPolyline.setMap(map);
+      // route click listeners, different one on each step
+      google.maps.event.addListener(stepPolyline, 'click', function(evt) {
+        infowindow.setContent("you clicked on the route<br>" + evt.latLng.toUrlValue(6));
+        infowindow.setPosition(evt.latLng);
+        infowindow.open(map);
+      })
+    }
+  }
+  map.fitBounds(bounds);
 }
