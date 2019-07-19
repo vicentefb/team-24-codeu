9f91450099efd4ad1cbf373799cc9ca71979159f
Eva Lynch <evaslynch01@gmail.com>
2019-07-19 02:34:30 -0500

    updated web design

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -105,8 +105,7 @@ function buildMessageDiv(message) {
   const headerDiv = document.createElement('div');
   headerDiv.classList.add('message-header');
   headerDiv.appendChild(document.createTextNode(
-      message.user + ' - ' + new Date(message.timestamp)));
-
+    message.user + ' - ' + new Date(message.timestamp)));
   const bodyDiv = document.createElement('div');
   bodyDiv.classList.add('message-body');
   bodyDiv.innerHTML = message.text;
